6786c847a1e8ce8ae1803bd26e2fcb6522ceb5bd
nikolaystankov1 <nikolay.stankov1@gmail.com>
2024-02-07 13:23:53 +0200

    adding - id_generator, invalid command, params, command factory; updating - app_data, view package and route command

---
+++ Logistics APP/commands/create_customer.py
@@ -0,0 +1,19 @@
+from core.application_data import ApplicationData
+from commands.validation_helpers import validate_params_count
+
+
+class CreateCustomerCommand:
+    def __init__(self, *params, app_data: ApplicationData):
+        validate_params_count(params, 2, 'CreateCustomer')
+        self._params = params
+        self._app_data = app_data  
+
+    def execute(self):
+        name, number = self._params  
+        
+        if self._app_data.find_customer(number):  
+            raise ValueError('Customer with that number already exists!')  
+        
+        self._app_data.create_customer(name, number)
+        
+        return f'New customer with {name} and {number} created!'  
