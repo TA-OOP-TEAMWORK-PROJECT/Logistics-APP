f99c0b7b793f57b4ab62f759b215f1cb98c889b0
Snezhi <snezhana.petrova1@gmail.com>
2024-02-07 15:12:08 +0200

    Changes on Truck super() Inheritance and class methods

--- Logistics APP/models/actros.py
+++ Logistics APP/models/actros.py
@@ -3,11 +3,13 @@ from truck import Truck
 
 class Actros(Truck):
     vehicle_ids = tuple(range(1026, 1041))
+    truck_brand = "Actros"
+    capacity_kg = 26000
+    max_range_km = 13000
 
     def __init__(self, truck_id):
         if truck_id not in Actros.vehicle_ids:
             raise ValueError(f"Truck ID {truck_id} is not valid for Actros trucks!")
-        truck_brand = "Actros"
-        capacity_kg = 26000
-        max_range_km = 13000
-        super().__init__(truck_id, truck_brand, capacity_kg, max_range_km)
+        super().__init__(truck_id, truck_brand=Actros.truck_brand,
+                         capacity_kg=Actros.capacity_kg, max_range_km=Actros.max_range_km)
+
--- Logistics APP/models/scania.py
+++ Logistics APP/models/scania.py
@@ -3,11 +3,12 @@ from truck import Truck
 
 class Scania(Truck):
     vehicle_ids = tuple(range(1001, 1011))
+    truck_brand = "Scania"
+    capacity_kg = 42000
+    max_range_km = 8000
 
     def __init__(self, truck_id):
         if truck_id not in Scania.vehicle_ids:
             raise ValueError(f"Truck ID {truck_id} is not valid for Scania trucks!")
-        truck_brand = "Scania"
-        capacity_kg = 42000
-        max_range_km = 8000
-        super().__init__(truck_id, truck_brand, capacity_kg, max_range_km)
+        super().__init__(truck_id, truck_brand=Scania.truck_brand,
+                         capacity_kg=Scania.capacity_kg, max_range_km=Scania.max_range_km)
