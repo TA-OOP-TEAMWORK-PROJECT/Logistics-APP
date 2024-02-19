398fdad9b19ba9160e9f04d12759bbf688a8c752
Snezhi <snezhana.petrova1@gmail.com>
2024-02-20 00:02:54 +0200

    View route in progress

--- Logistics APP/commands/view_route_in_progress.py
+++ Logistics APP/commands/view_route_in_progress.py
@@ -1,3 +1,5 @@
+from datetime import datetime,timedelta
+
 from core.application_data import ApplicationData
 from commands.validation_helpers import validate_params_count
 from models.route import Route
@@ -5,7 +7,32 @@ from models.route import Route
 class ViewRoutesInProgressCommand:          #Use case 3
     def __init__(self, params, app_data: ApplicationData):
         self.app_data = app_data
+        self.start = params[0]
+        self.end = params[1]
 
     def execute(self):
-        routes_info = [route.info() for route in self.app_data._routes if route.is_in_progress()]
-        return "\n".join(routes_info) if routes_info else "No routes in progress."
+
+        suitable_routes = self.search_routes(self.app_data._routes)
+        if suitable_routes:
+            return self.info(suitable_routes)
+
+
+    def search_routes(self, routes):
+        current_routes = []
+        for r in routes:
+            cities = list(r.keys())
+            if self.start in cities and self.end in cities:
+                if cities.index(self.start) < cities.index(self.end):
+                    current_routes.append(r)
+
+        return routes
+
+    def info(self, suitable_routes):
+        result = ''
+
+        for route in suitable_routes:
+            for k, v in route.items():
+                result += f' {k} ({v.strftime('%b %dth %H:%Mh')}) →'
+
+        return result.strip('→')
+
